8772c3da47b4dfa550d458a03bf5df1e1feeb337
Sancho Amaral Simões <ssimoes@ipn.pt>
2022-05-27 16:19:22 +0100

    MASTER: Add missing red card table.

---
+++ src/ScoresDEI/src/app/java/DataLayer/Model/EventRedCard.java
@@ -0,0 +1,9 @@
+package DataLayer.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity(name = "EventRedCard")
+@Table(name = "EventRedCard")
+public class EventRedCard extends Event{
+}
