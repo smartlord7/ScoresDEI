f4f7547e8d42223b2ac6f0c443715fe389c758e5
Sancho Amaral Simões <ssimoes@ipn.pt>
2022-05-27 15:29:42 +0100

    MASTER: Specify event inheritance strategy type.

--- src/ScoresDEI/src/app/java/DataLayer/Model/Event.java
+++ src/ScoresDEI/src/app/java/DataLayer/Model/Event.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import java.util.Date;
 
 @MappedSuperclass
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
