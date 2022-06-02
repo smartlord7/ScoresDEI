cb1de2c6da57a81404b0037057b7b78df148ab63
Sancho Amaral Simões <ssimoes@ipn.pt>
2022-06-02 18:56:35 +0100

    STABLE: Implement player grid.

---
+++ src/ScoresDEI/src/app/java/Main/PresentationLayer/Controller/Player/PlayerViewController.java
@@ -0,0 +1,29 @@
+package Main.PresentationLayer.Controller.Player;
+
+import Main.BusinessLayer.Player.PlayerReader;
+import Main.BusinessLayer.Player.PlayerWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import static Main.Util.ApplicationConst.APP_NAME;
+
+@Controller
+@RequestMapping(path = APP_NAME + "/player")
+public class PlayerViewController {
+    @Autowired
+    private PlayerReader reader;
+
+    @Autowired
+    private PlayerWriter writer;
+
+    @GetMapping
+    public ModelAndView getAll(Model model) {
+        model.addAttribute("players", reader.getAll());
+
+        return new ModelAndView("player/index");
+    }
+}
