780e2501bddbf6776f9f2ef559ae55406a4dea76
Razoor <tiagofilipe122@gmail.com>
2022-06-02 02:32:58 +0100

    STABLE: ADD HEADER.

--- src/ScoresDEI/src/app/java/BusinessLayer/Player/PlayerWriter.java
+++ src/ScoresDEI/src/app/java/BusinessLayer/Player/PlayerWriter.java
@@ -1,3 +1,14 @@
+/**------------scoreDEI: Resultados desportivos em direto - WEB FRAMEWORK------------
+ University of Coimbra
+ Degree in Computer Science and Engineering
+ Sistemas Distribuidos
+ 3rd year, 2nd semester
+ Authors:
+ Sancho Amaral Simões, 2019217590, uc2019217590@student.uc.pt
+ Tiago Filipe Santa Ventura, 2019243695, uc2019243695@student.uc.pt
+ Coimbra, 31th May 2022
+ */
+
 package BusinessLayer.Player;
 
 import BusinessLayer.Player.DTO.PlayerCreateDTO;
